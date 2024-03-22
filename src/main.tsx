9f7d1bd573cb04c59be8009d7754c7159ab22dff
inverted-capital <69952973+inverted-capital@users.noreply.github.com>
2024-03-22 14:25:27 +1300

    minor

--- src/main.tsx
+++ src/main.tsx
@@ -5,9 +5,10 @@ import Provider from './react/Provider.tsx'
 
 const url = import.meta.env.VITE_API_URL
 console.log('api url:', url)
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <Provider url={url}>
+    <Provider>
       <App />
     </Provider>
   </React.StrictMode>
