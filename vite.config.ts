ca25839a26d95079710358be78ed1db113297732
inverted-capital <69952973+inverted-capital@users.noreply.github.com>
2024-03-22 14:18:30 +1300

    ghactions

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,8 @@ if (!process.env.VITE_API_URL) {
   process.env.VITE_API_URL = 'http://localhost:8000'
 }
 
+console.log('VITE_API_URL', process.env.VITE_API_URL)
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
